f740881d5452291ad0c1abc44caf618ff8de229e
Lauren Bannon <laurenbannon98@gmail.com>
2019-11-17 16:13:05 +0000

    Adding searching layout to toolbar

--- app/src/main/java/com/example/mealplanner/SearchActivity.java
+++ app/src/main/java/com/example/mealplanner/SearchActivity.java
@@ -2,23 +2,56 @@ package com.example.mealplanner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SearchActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    ArrayAdapter adapter;
+    ListView listView;
+    TextView emptyView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.searchToolbar);
+        toolbar = (Toolbar) findViewById(R.id.searchToolbar);
         setSupportActionBar(toolbar);
+
+        listView = (ListView) findViewById(R.id.list);
+        emptyView = (TextView) findViewById(R.id.emptyView);
+
+        setSupportActionBar(toolbar);
+
+        adapter = new ArrayAdapter(SearchActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.months));
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(SearchActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView.setEmptyView(emptyView);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
 
@@ -51,4 +84,51 @@ public class SearchActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_bar_menu, menu);
+        MenuItem mSearch = menu.findItem(R.id.action_search);
+        SearchView mSearchView = (SearchView) mSearch.getActionView();
+        mSearchView.setQueryHint("Search");
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+//
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.search_bar_menu, menu);
+//
+//        MenuItem searchMenu = menu.findItem(R.id.action_search);
+//
+//        SearchView searchView = (SearchView) searchMenu.getActionView();
+//        searchView.setQueryHint("Search");
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                adapter.getFilter().filter(newText);
+//                return true;
+//            }
+//        });
+//
+//        super.onCreateOptionsMenu(menu);
+//        return true;
+//
+//    }
+
 }
