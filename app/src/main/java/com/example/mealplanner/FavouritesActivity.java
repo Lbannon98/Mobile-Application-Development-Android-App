8112bf144091560ee19140498df69affe3606075
Lauren Bannon <laurenbannon98@gmail.com>
2019-11-18 18:43:33 +0000

    Fixing code to use intents

--- app/src/main/java/com/example/mealplanner/FavouritesActivity.java
+++ app/src/main/java/com/example/mealplanner/FavouritesActivity.java
@@ -3,60 +3,35 @@ package com.example.mealplanner;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class MainActivity extends AppCompatActivity {
-
+public class FavouritesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.favourites_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
                 switch(menuItem.getItemId()) {
                     case R.id.nav_search:
-
-                        Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+                        Intent searchIntent = new Intent(FavouritesActivity.this, SearchActivity.class);
                         searchIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(searchIntent);
-
                         break;
-
-                    case R.id.nav_favourites:
-
-                        Intent favouritesIntent = new Intent(MainActivity.this, FavouritesActivity.class);
-                        favouritesIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(favouritesIntent);
-
-                        break;
-
                     case R.id.nav_meal_plan:
-
-                        Intent mealPlanIntent = new Intent(MainActivity.this, MealPlanActivity.class);
+                        Intent mealPlanIntent = new Intent(FavouritesActivity.this, MealPlanActivity.class);
                         mealPlanIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(mealPlanIntent);
-
                         break;
-
                 }
-
                 return false;
             }
         });
-
     }
-
 }
